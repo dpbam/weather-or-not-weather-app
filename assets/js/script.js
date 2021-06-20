9a53b1d9759aca9194ff6c83a08155e976eb6d08
Derek Phelps <derekphtpt@gmail.com>
2021-06-20 17:18:11 -0600

    header background color gradient and search bar and field width

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
 // use moment.js to get the dates for displaying the weather on today AND the five day forcast
-const elem = document.createElement("div");
-const elemText = document.createTextNode("This is a div");
+// const elem = document.createElement("div");
+// const elemText = document.createTextNode("This is a div");
 
-elem.appendChild(elemText);
+// elem.appendChild(elemText);
 
-document.body.appendChild(elem);
+// document.body.appendChild(elem);
 
-elem.classList.add("heading");
-elem.id = "newDiv";
-elem.setAttribute("option", "test");
+// elem.classList.add("heading");
+// elem.id = "newDiv";
+// elem.setAttribute("option", "test");
 
-const profileImg = document.createElement("img");
-profileImg.src = "http://placekitten.com/200/300";
-document.body.appendChild(profileImg);
-profileImg.style.width = "50px";
+// const profileImg = document.createElement("img");
+// profileImg.src = "http://placekitten.com/200/300";
+// document.body.appendChild(profileImg);
+// profileImg.style.width = "50px";
 
-const inputElem = document.createElement("input");
+// const inputElem = document.createElement("input");
 
-inputElem.type = "checkbox";
+// inputElem.type = "checkbox";
 
-const labelElem = document.createElement("label");
-const labelText = document.createTextNode("Click Me");
+// const labelElem = document.createElement("label");
+// const labelText = document.createTextNode("Click Me");
 
-labelElem.appendChild(labelText);
-labelElem.appendChild(inputElem);
+// labelElem.appendChild(labelText);
+// labelElem.appendChild(inputElem);
 
-document.body.appendChild(labelElem);
+// document.body.appendChild(labelElem);
