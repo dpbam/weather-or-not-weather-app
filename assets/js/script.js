15494809b96304fa5fb4b2b992e14205d98bef5f
Derek Phelps <derekphtpt@gmail.com>
2021-06-23 14:59:03 -0600

    put current date in parentheses

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,24 @@
 // elem.id = "newDiv";
 // elem.setAttribute("option", "test");
 const currentDate = moment();
+const dayOne = moment().add(1, 'day');
+const dayTwo = moment().add(2, 'day');
+const dayThree = moment().add(3, 'day');
+const dayFour = moment().add(4, 'day');
+const dayFive = moment().add(5, 'day');
+
 var city = "";
 
 document.getElementById("currentDate").innerHTML =
-  currentDate.format("MM/DD/YYYY");
+  currentDate.format("[ (]MM/DD/YYYY[)]");
+
+document.getElementById("day-one").innerHTML = dayOne.format("MM/DD/YYYY")
+document.getElementById("day-two").innerHTML = dayTwo.format("MM/DD/YYYY")
+document.getElementById("day-three").innerHTML = dayThree.format("MM/DD/YYYY")
+document.getElementById("day-four").innerHTML = dayFour.format("MM/DD/YYYY")
+document.getElementById("day-five").innerHTML = dayFive.format("MM/DD/YYYY")
+
+
 
 let weather = {
   apiKey: "6e6ec74027adce6a58ed16bac77822ab",
@@ -28,16 +42,31 @@ let weather = {
       .then((data) => this.displayWeather(data));
   },
   displayWeather: function (data) {
+    console.log(data)
     const { name } = data;
     const { icon } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     console.log(name, icon, temp, humidity, speed);
     document.querySelector(".city").innerText =
-      name + "(" + "#currentDate" + ")";
-    // document.querySelector(".icon").src =
-    //   "https://openweathermap.org/img/wn/" + icon + ".png";
+      name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".icon").innerText = icon;
+    document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "MPH";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
   },
 };
 
-// document.getElementsByClassName("city-name").innerHTML =
+var searchButton = document.querySelector(".search-btn")
+searchButton.addEventListener("click", function(clickEvent) {
+var city = document.getElementById("city-name").value
+console.log(city)
+weather.fetchWeather(city);
+
+})
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// has the uv index here so 
+
